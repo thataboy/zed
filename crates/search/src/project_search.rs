e853ae88ca6fd9bc940f674649f53faf61953e02
thataboy <dearbart@yahoo.com>
2024-09-25 20:12:06 -0700

    Add border_color to highlighted_range

--- crates/search/src/project_search.rs
+++ crates/search/src/project_search.rs
@@ -1150,7 +1150,7 @@ impl ProjectSearchView {
                 }
                 editor.highlight_background::<Self>(
                     &match_ranges,
-                    |theme| theme.search_match_background,
+                    |theme| (theme.search_match_background, theme.border_selected),
                     cx,
                 );
             });
